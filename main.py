7abe526f8cc3d9b2e2bb168debb479700334c1bc
LeopardI986 <tema.diyakov@gmail.com>
2021-03-10 20:10:55 +0400

    array

--- main.py
+++ main.py
@@ -13,12 +13,42 @@ def multi(a, b):
     return a * b
 
 
+def my_max(array):
+    # return max(array)
+    cur_max = array[0]
+    for d in array[1:]:
+        if d > cur_max:
+            cur_max = d
+    return cur_max
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-   ad = 5
-   bd = 3
-   c = multi(ad, bd)
-   print(c)
+    # a = 'sdjkfdkjf'
+    # b = "asdfdfadsf"
+    # print(a, len(a), a.endswith('kjz'))
+    # t = True
+    # if t and len(a) >= 9:
+    #     print('Tryeeeee')
+    # for c in a:
+    #     print(c)
+    arr = [-1, -2, -88, -35, -34, -10, -23, -12, -99, -11]
+    arr2 = [5]
+    # f = 0
+    #
+    # for i in arr:
+    #     f += i
+    #     print(f)
+    # print(min(arr))
+    odd_sum = not_odd_sum = 0
+    for g in arr:
+        if g % 2 == 1:
+            not_odd_sum += g
+        else:
+            odd_sum += g
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-# print_hi("Artem")
+    print(f'odd sum: {odd_sum}, not odd summ {not_odd_sum} ')
+
+    m = my_max(arr2)
+    print(f'max of array = {m}')
